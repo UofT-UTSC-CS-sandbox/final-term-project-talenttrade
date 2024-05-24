c9ba547e3873ad02c970a9c9d3565f548bbbe7e8
Antonio Ferreras <antfs10@gmail.com>
2024-05-24 00:58:21 -0400

    Sample app to show connection of front and backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-q1g9$j0k=9_o4=ug!--#dbsl97l5%#wr-7v*lsy6f@z9&4jik4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -37,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'sampleapp',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,89 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import host from "./utils/links";
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const incrementCount = async () => {
+    console.log('incrementing count')
+    try {
+      const response = await fetch(`${host}/sampleapp/increment-count/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      if (response.ok) {
+        // Handle successful response
+        const data = await response.json();
+        setCount(data.count);
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle network error
+    }
+  }
+
+  const resetCount = async () => {
+    console.log('resetting count')
+    try {
+      const response = await fetch(`${host}/sampleapp/reset-count/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      if (response.ok) {
+        // Handle successful response
+        const data = await response.json();
+        setCount(data.count);
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle network error
+    }
+  }
+  const getCount = async () => {
+    console.log('getting count')
+    try {
+      const response = await fetch(`${host}/sampleapp/get-count/`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      if (response.ok) {
+        // Handle successful response
+        const data = await response.json();
+        setCount(data.count);
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle network error
+    }
+  }
+
+  useEffect(() => {
+    getCount();
+  }, []);
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>React + Django backend</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={incrementCount}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={resetCount}>
+          reset count
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
