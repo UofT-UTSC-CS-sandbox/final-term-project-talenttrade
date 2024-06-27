d70893b5752c8ae7c7663b59cca828b44b163238
Joyce Ang <joyce.ang@mail.utoronto.ca>
2024-06-27 15:03:47 -0400

    SCRUM-52 and SCRUM-54 worked on the frontend portion of ratings and filtering by offers

---
+++ frontend/src/components/filterBar.tsx
@@ -0,0 +1,13 @@
+import Ratings from "./ratings";
+import OfferFilter from "./offerFilter";
+
+const FilterBar = () => {
+  return (
+    <div>
+      <OfferFilter />
+      <Ratings />
+    </div>
+  );
+};
+
+export default FilterBar;
